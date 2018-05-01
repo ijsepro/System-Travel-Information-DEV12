88ba100cbbbbcf11e6602cb438b7b9f181a5e234
thisu96 <thisarudinuwanthi@gmail.com>
2018-05-01 10:45:12 +0530

    dtos

---
+++ System-Travel-Information/Back-End/src/main/java/lk/edu/ijse/tis/dto/HotelDTO.java
@@ -0,0 +1,80 @@
+package lk.edu.ijse.tis.dto;
+
+public class Hotel {
+
+    private int hotelId;
+    private String hotelName;
+    private String hotelType;
+    private String hotelDescription;
+    private String hotelDistrict;
+    private String hotelTown;
+
+    public Hotel() {
+
+    }
+
+    public Hotel(int hotelId, String hotelName, String hotelType, String hotelDescription, String hotelDistrict, String hotelTown) {
+        this.setHotelId(hotelId);
+        this.setHotelName(hotelName);
+        this.setHotelType(hotelType);
+        this.setHotelDescription(hotelDescription);
+        this.setHotelDistrict(hotelDistrict);
+        this.setHotelTown(hotelTown);
+    }
+
+    public Hotel(String hotelName, String hotelType, String hotelDescription, String hotelDistrict, String hotelTown) {
+        this.setHotelName(hotelName);
+        this.setHotelType(hotelType);
+        this.setHotelDescription(hotelDescription);
+        this.setHotelDistrict(hotelDistrict);
+        this.setHotelTown(hotelTown);
+    }
+
+    public int getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(int hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public String getHotelType() {
+        return hotelType;
+    }
+
+    public void setHotelType(String hotelType) {
+        this.hotelType = hotelType;
+    }
+
+    public String getHotelDescription() {
+        return hotelDescription;
+    }
+
+    public void setHotelDescription(String hotelDescription) {
+        this.hotelDescription = hotelDescription;
+    }
+
+    public String getHotelDistrict() {
+        return hotelDistrict;
+    }
+
+    public void setHotelDistrict(String hotelDistrict) {
+        this.hotelDistrict = hotelDistrict;
+    }
+
+    public String getHotelTown() {
+        return hotelTown;
+    }
+
+    public void setHotelTown(String hotelTown) {
+        this.hotelTown = hotelTown;
+    }
+}
