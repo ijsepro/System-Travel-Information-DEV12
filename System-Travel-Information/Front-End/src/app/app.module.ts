d8a82c1ef7c81933e8df4e12723292cce231788e
AmaliLakshani <amali8463@gmail.com>
2018-06-09 15:11:24 +0530

    Validation Register User

--- System-Travel-Information/Front-End/src/app/app.module.ts
+++ System-Travel-Information/Front-End/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import {MdDialogModule} from '@angular/material';
 
 
 import { AppComponent } from './app.component';
@@ -9,16 +10,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NewHotelComponent } from './new-hotel/new-hotel.component';
 import { NewPlaceComponent } from './new-place/new-place.component';
 import {MaterialModule} from './material.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {PlaceService} from './new-place/place.service';
 import {HotelService} from './new-hotel/hotel.service';
- import {HomepageComponent} from './homepage/homepage.component';
-// import { NewComponentComponent } from './new-component/new-component.component';
+import {HomepageComponent} from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
 import { RegistationComponent } from './registation/registation.component';
-import {SignInComponent} from "./sign-in/sign-in.component";
-import {SignUpComponent} from "./sign-up/sign-up.component";
+import {SignInComponent} from './sign-in/sign-in.component';
+import {SignUpComponent} from './sign-up/sign-up.component';
+import { TestComponent } from './test/test.component';
 
 
 const approutes: Routes = [
@@ -30,7 +31,7 @@ const approutes: Routes = [
   { path: 'registation', component: RegistationComponent},
   { path: 'sign-up', component: SignUpComponent},
   { path: 'sign-in', component: SignInComponent},
-  { path : '', redirectTo:'/sign-in', pathMatch : 'full'}
+  // { path : '', redirectTo:'/sign-in', pathMatch : 'full'}
 
   ];
 
@@ -41,19 +42,22 @@ const approutes: Routes = [
     NewHotelComponent,
     NewPlaceComponent,
     HomepageComponent,
-    // NewComponentComponent,
     LoginComponent,
     SignInComponent,
     SignUpComponent,
     RegistationComponent,
+    TestComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
     MaterialModule,
     RouterModule.forRoot(approutes),
+    // MdDialogModule
+
 
   ],
   providers: [PlaceService, HotelService ],
--- System-Travel-Information/Front-End/src/app/sign-in/sign-in.component.ts
+++ System-Travel-Information/Front-End/src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms' ;
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
+  rForm: FormGroup;
+  post: any;
+  username: String = '';
+  titleAlert: String = 'This Field Is required';
 
-  constructor() { }
+  constructor(fb: FormBuilder) {
+    this.rForm = fb.group({
+      'username' : [null, Validators.required],
+      // 'username' : [null, Validators.compose([Validators.required, Validators.minLength(4), Validators.maxLength(20)])],
+
+    });
+
+
+  }
 
   ngOnInit() {
   }
-
+  validation(valiForm) {
+    this.username = valiForm.username;
+}
 }
